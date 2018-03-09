469b8c9945248f8570f68550fafa59ae0d4952fb
Marta Aguilar <martaguilar.soto@gmail.com>
2018-03-09 14:00:32 +0100

    create structure react

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img />
+          <h1 className="App-title"></h1>
+        </header>
+        <p className="App-intro">
+
+        </p>
+      </div>
+    );
+  }
+}
